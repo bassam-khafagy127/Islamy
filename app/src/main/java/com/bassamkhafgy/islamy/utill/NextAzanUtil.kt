1582eadfeabe60c8e742c7c507323a8253664a77
bassamkhafagy <bassamkhafgy127@gmail.com>
2023-06-08 20:17:03 +0300

    next prayer is almost don

--- app/src/main/java/com/bassamkhafgy/islamy/utill/NextAzanUtil.kt
+++ app/src/main/java/com/bassamkhafgy/islamy/utill/NextAzanUtil.kt
@@ -9,6 +9,8 @@ import java.time.format.DateTimeFormatter
 fun getNextAzanTitle(prayerTimes: List<PrayerTime>, currentTime: String): PrayerTime {
     val timeFormatter = DateTimeFormatter.ofPattern("h:mm a")
 
+    val currentCheckValue: LocalTime = LocalTime.parse(currentTime, timeFormatter)
+
     val fajr: LocalTime = LocalTime.parse(prayerTimes[0].time, timeFormatter)
     val sunrise: LocalTime = LocalTime.parse(prayerTimes[1].time, timeFormatter)
     val dhuhr: LocalTime = LocalTime.parse(prayerTimes[2].time, timeFormatter)
@@ -16,19 +18,20 @@ fun getNextAzanTitle(prayerTimes: List<PrayerTime>, currentTime: String): Prayer
     val maghrib: LocalTime = LocalTime.parse(prayerTimes[4].time, timeFormatter)
     val isha: LocalTime = LocalTime.parse(prayerTimes[5].time, timeFormatter)
 
-//    when {
-//        dhuhr.isAfter(asr) -> {
-//            Log.d("PrayeTime:", "$dhuhr dhuhr is after $asr asr")
-//
-//        }
-//
-//        dhuhr.isBefore(asr) -> {
-//            Log.d("PrayeTime:", "$dhuhr dhuhr is before $asr asr")
-//        }
-//
-//        dhuhr == asr -> {
-//            Log.d("PrayeTime:", "Both is equal")
-//        }
-//    }
+    when {
+        currentCheckValue.isAfter(asr) -> {
+            Log.d("PrayeTime:", "$currentCheckValue currentCheckValue is after $asr asr")
+
+        }
+
+        currentCheckValue.isBefore(asr) -> {
+            Log.d("PrayeTime:", "$currentCheckValue currentCheckValue is before $asr asr")
+        }
+
+        currentCheckValue == asr -> {
+            Log.d("PrayeTime:", "Both is equal")
+        }
+    }
+
     return prayerTimes[4]
 }
